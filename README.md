4b362fb59a01bae33e37cffdd5309baa8cee3953
Ian Knighton <ian@knighton.dev>
2020-12-01 13:50:13 -0700

    Basic Node and Pod Scanning

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bitbar-kubectl
-This is a plugin for bitbar/SwiftBar that checks information on you kubernetes instance using kubectl.
+
+This is a simple BitBar/SwiftBar plugin that uses `kubectl` to display the status of your current kubernetes configuration. 
+
+I needed a quick way to see how many nodes and pods I had running in my cluster and this is easier than having an aliased command in zsh.
+
+## Usage
+
+You will need to have [BitBar](https://github.com/matryer/bitbar) or [SwiftBar](https://github.com/swiftbar/SwiftBar) installed first. Once you have either installed, you should be able to add this to your plugin directory and it will fire from there. I do not have it added to the official library yet because I'm not sure it provides enough value at this point.
+
+At it's current state, it relies on your current configuration and just pulls back a total of all running pods and ready nodes. I hope to add more configuration in future versions. 
---
+++ kubectl.5m.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# <bitbar.title>Kubectl Info</bitbar.title>
+# <bitbar.version>v0.1</bitbar.version>
+# <bitbar.author>Ian Knighton</bitbar.author>
+# <bitbar.author.github>IanKnighton</bitbar.author.github>
+# <bitbar.desc>Provides quick access to information on pods and nodes in your kubernetes cluster.<bitbar.desc>
+# <bitbar.image></bitbar.image>
+# <bitbar.dependencies>kubectl</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/IanKnighton/bitbar-kubectl</bitbar.abouturl>
+
+LANG="en_US.UTF-8"
+# You may need to update the PATH to include minikube and kubectl
+PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export LANG PATH
+
+CURRENT_NODES=`kubectl get nodes`
+CURRENT_PODS=`kubectl get pods`
+
+RUNNING_NODE_COUNT=`echo "$CURRENT_NODES" | grep Ready | wc -l | sed -e 's/^[[:space:]]*//'`
+RUNNING_POD_COUNT=`echo "$CURRENT_PODS" | grep Running | wc -l | sed -e 's/^[[:space:]]*//'`
+
+echo "Nodes: $RUNNING_NODE_COUNT"
+echo "Pods: $RUNNING_POD_COUNT"
